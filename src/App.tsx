50d6105d3851886686b37917146301ac6de327e2
Cláudio Roberto <claudioroberto0707@outlook.com>
2026-02-28 23:51:24 -0300

    style: apply oceanic theme and configure vite proxy for cors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
+import { Toaster } from './components/ui/sonner'
 import { AuthProvider } from './contexts/AuthContext'
+import Login from './pages/Login'
 
 function App() {
 
 
   return (
     <>
-     <AuthProvider>
-    <h1>teste</h1>
-    </AuthProvider>
+      <Toaster />
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Rota Pública */}
+            <Route path="/login" element={<Login />} />
+            {/* <Route 
+              path="/dashboard" 
+              element={<Dashboard />} 
+            /> */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
